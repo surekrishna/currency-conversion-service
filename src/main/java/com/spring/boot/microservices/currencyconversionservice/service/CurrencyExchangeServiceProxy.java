9bf05b0113bb3bc43042f6cde13ad082aa0c4012
surekrishna <surekrrishna@gmail.com>
2018-09-19 23:57:18 +0530

    Setting up zuul api gateway between microservice invocations making service calls through netflx-zuul-api-gateway-server

--- src/main/java/com/spring/boot/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/spring/boot/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.spring.boot.microservices.currencyconversionservice.bean.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
@@ -15,7 +15,7 @@ public interface CurrencyExchangeServiceProxy {
 	 * This method(every thing is same) is taken from currency-exchange application to call the rest service using
 	 * openfeign to reduce code and maintainance  
 	 */
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean getCurrencyExchangeValue(@PathVariable String from, @PathVariable String to);
 
 }
